86553e6f2eda717a3acd21037ef8f5f9aa35a438
edgarttrr <35516028+edgarttrr@users.noreply.github.com>
2018-01-24 18:48:19 -0800

    Update cone.js

--- Assingment 3/cone.js
+++ Assingment 3/cone.js
@@ -4,6 +4,15 @@ function init() {
     var canvas = document.getElementById( "webgl-canvas" );
 
     gl = WebGLUtils.setupWebGL( canvas );
+    
+    attribute vec4 vPosition;
+
+void main() 
+{
+     gl_Position = vPosition;
+}
+    
+    
 
     if ( !gl ) {
         alert("Unable to setup WebGL");
@@ -13,6 +22,10 @@ function init() {
     gl.clearColor( 1.0,  1.0,  0.0,  1.9,);
 
     render();
+}
+void main()
+{
+    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);
 }
 
 function render() {
