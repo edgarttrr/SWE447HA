7d9c10ce2b2f50f0bd114a394d58ea2c00e5ab4d
edgarttrr <35516028+edgarttrr@users.noreply.github.com>
2018-01-29 18:03:09 -0800

    Update Square.js

--- Common/Square.js
+++ Common/Square.js
@@ -18,7 +18,7 @@ function Square(gl, vertexShaderId, fragmentShaderId) {
 	this.positions = {
 		values : new Float32Array([
 		    // Front face
-		    0.0, 1.0, // Vertex 0
+		    0.0, 0.0, // Vertex 0
 		    1.0, 0.0, // Vertex 1
 		    1.0, 1.0, // Vertex 2
 		    0.0, 1.0  // Vertex 3
