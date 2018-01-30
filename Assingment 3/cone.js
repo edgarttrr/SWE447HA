1cb7920cbe2e9d767addb8d4bf35282ee63a62d9
edgarttrr <35516028+edgarttrr@users.noreply.github.com>
2018-01-29 17:24:13 -0800

    Update cone.js

--- Assingment 3/cone.js
+++ Assingment 3/cone.js
@@ -16,14 +16,14 @@ function init() {
 
     gl.clearColor( 1.0,  0.0,  1.0,  1.0,);
 
-    render();
+    cone.render();
     
 }
 
 
-function render() {
+function cone.render() {
     gl.clear( gl.COLOR_BUFFER_BIT );
-  cone.render();
+  
     
 }
 
