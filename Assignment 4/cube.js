44933eee1aa50e25e96061fd9ec3e5d4bfb74c78
edgarttrr <35516028+edgarttrr@users.noreply.github.com>
2018-02-08 21:52:29 -0800

    Update cube.js

--- Assignment 4/cube.js
+++ Assignment 4/cube.js
@@ -2,25 +2,8 @@
 var cube = undefined;
 var gl = undefined;
 var angle = 0;
- const faceColors = [
-    [1.0,  1.0,  1.0,  1.0],    // Front face: white
-    [1.0,  0.0,  0.0,  1.0],    // Back face: red
-    [0.0,  1.0,  0.0,  1.0],    // Top face: green
-    [0.0,  0.0,  1.0,  1.0],    // Bottom face: blue
-    [1.0,  1.0,  0.0,  1.0],    // Right face: yellow
-    [1.0,  0.0,  1.0,  1.0],    // Left face: purple
-  ];
-
-  var colors = [];
-
-  for (var j = 0; j < faceColors.length; ++j) {
-    const c = faceColors[j];
-
-    
-    colors = colors.concat(c, c, c, c);
-  }
-
-  const colorBuffer = gl.createBuffer();
+ 
+  
 function init() {
   var canvas = document.getElementById( "webgl-canvas" );
 
@@ -42,8 +25,7 @@ function init() {
 function render() {
   gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT );
  
-  gl.bindBuffer(gl.ARRAY_BUFFER, colorBuffer);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(colors), gl.STATIC_DRAW);
+  
   
   angle += 2.0; // degrees
 
