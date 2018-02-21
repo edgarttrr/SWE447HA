4643cb3b40293a8e3ec0ebf1de45bbafe5c93252
edgarttrr <35516028+edgarttrr@users.noreply.github.com>
2018-02-20 21:29:28 -0800

    Update SolarSystem.js

--- SolarSystem/SolarSystem.js
+++ SolarSystem/SolarSystem.js
@@ -1,67 +1,67 @@
 var SolarSystem = {
   Sun : {
-    radius : 10,  
+    radius : 3,  
     distance : 0,
     year : 0,
     color : [ 1.0, 1.0, 0.0, 1.0 ]
   },
   Mercury : {
-    radius : 0.0553,
-    distance : 0.387,
+    radius : 0.24397,
+    distance : 5.79,
     year : 0.241,
     color : [ 1.0, 0.0, 0.0, 1.0 ]
   },
   Venus : {
-    radius : 0.949,
-    distance : 0.723,
+    radius : 0.60518,
+    distance : 10.82,
     year : 0.615,
     color : [ 1.0, 0.0, 1.0, 1.0 ]
   },
   Earth : {
-    radius : 5,
-    distance : 1,
+    radius : 0.63781,
+    distance : 14.96,
     year : 1,
     color : [ 0.0, 0.0, 1.0, 1.0 ]
   },
   Moon : {
-    radius : 0.2724,
+    radius : 0.17381,
     distance : 0.00257,
     year : 0.0748,
     color : [ 0.5, 0.5, 0.5, 1.0 ]
   },
   Mars : {
-    radius : 0.532,
-    distance : 1.52,
+    radius : 0.33962,
+    distance : 22.79,
     year : 1.88,
     color : [ 1.0, 0.0, 0.0, 1.0 ]
   },
   Jupiter : {
-    radius : 11.21,
-    distance : 5.20,
+    radius : 7.1492,
+    distance : 77.83,
     year : 11.9,
     color : [ 1.0, 153/255, 0.0, 1.0 ]
   },
   Saturn : {
-    radius : 9.45,
-    distance : 9.58,
+    radius : 6.0268,
+    distance : 142.70,
     year : 29.4,
     color : [ 1.0, 1.0, 0.0, 1.0 ]
   },
   Uranus : {
-    radius : 4.01,
-    distance : 19.20,
+    radius : 2.5559,
+    distance : 287.10,
     year : 83.7,
     color : [ 0.0, 0.1, 1.0, 1.0 ]
   },
   Neptune : {
-    radius : 3.88,
-    distance : 30.05,
+    radius : 2.4764,
+    distance : 444.971,
     year : 163.7,
     color : [ 0.0, 0.0, 1.0, 1.0 ]
   },
   Pluto : {
-    radius : 0.186,
-    distance : 39.48,
+    radius : 0.1195,
+    distance : 394.8,
     year : 247.9,
     color : [ 150/255, 75/255, 0.0, 1.0 ]
   }
