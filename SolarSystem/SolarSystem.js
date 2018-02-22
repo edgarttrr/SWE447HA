e16a900b1fb85d41f4d145a8ef3288f71a3f3313
edgarttrr <35516028+edgarttrr@users.noreply.github.com>
2018-02-21 22:21:49 -0800

    Update SolarSystem.js
    Kept hittting one other so updated distance agian.

--- SolarSystem/SolarSystem.js
+++ SolarSystem/SolarSystem.js
@@ -7,19 +7,19 @@ var SolarSystem = {
   },
   Mercury : {
     radius : 0.24397,
-    distance : 0.5,
+    distance : 5.0,
     year : 0.241,
     color : [ 1.0, 0.0, 0.0, 1.0 ]
   },
   Venus : {
     radius : 0.60518,
-    distance : 1.0,
+    distance : 6.0,
     year : 0.615,
     color : [ 1.0, 0.0, 1.0, 1.0 ]
   },
   Earth : {
     radius : 0.6378,
-    distance : 2.0,
+    distance : 7.0,
     year : 1,
     color : [ 0.0, 0.0, 1.0, 1.0 ]
   },
@@ -31,25 +31,25 @@ var SolarSystem = {
   },
   Mars : {
     radius : 0.3396,
-    distance : 3.0,
+    distance : 8.0,
     year : 1.88,
     color : [ 1.0, 0.0, 0.0, 1.0 ]
   },
   Jupiter : {
     radius : 0.71492,
-    distance : 7.0,
+    distance : 12.0,
     year : 11.9,
     color : [ 1.0, 153/255, 0.0, 1.0 ]
   },
   Saturn : {
     radius : 0.60268,
-    distance : 14.0,
+    distance : 24.0,
     year : 29.4,
     color : [ 1.0, 1.0, 0.0, 1.0 ]
   },
   Uranus : {
     radius : 0.25559,
-    distance : 28.0,
+    distance : 38.0,
     year : 83.7,
     color : [ 0.0, 0.1, 1.0, 1.0 ]
   },
