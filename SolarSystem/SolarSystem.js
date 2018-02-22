f82527190c1a6c281e9d321afd6dba8d043982ef
edgarttrr <35516028+edgarttrr@users.noreply.github.com>
2018-02-21 18:04:38 -0800

    Update SolarSystem.js
    Updated Km distance to make to round the numbers bigger since planets begin to hit one other.  and updated time since other planets are too slow in rotation.

--- SolarSystem/SolarSystem.js
+++ SolarSystem/SolarSystem.js
@@ -6,20 +6,20 @@ var SolarSystem = {
     color : [ 1.0, 1.0, 0.0, 1.0 ]
   },
   Mercury : {
-    radius : 0.24397,
-    distance : .0579,
+    radius : 0.244,
+    distance : .06,
     year : 0.241,
     color : [ 1.0, 0.0, 0.0, 1.0 ]
   },
   Venus : {
     radius : 0.60518,
-    distance : .1082,
+    distance : .11,
     year : 0.615,
     color : [ 1.0, 0.0, 1.0, 1.0 ]
   },
   Earth : {
     radius : 0.6378,
-    distance : .1496,
+    distance : .15,
     year : 1,
     color : [ 0.0, 0.0, 1.0, 1.0 ]
   },
@@ -30,39 +30,39 @@ var SolarSystem = {
     color : [ 0.5, 0.5, 0.5, 1.0 ]
   },
   Mars : {
-    radius : 0.3396,
+    radius : 0.34,
     distance : 2.279,
     year : 1.88,
     color : [ 1.0, 0.0, 0.0, 1.0 ]
   },
   Jupiter : {
     radius : 0.71492,
-    distance : 7.783,
+    distance : 7.9,
     year : 11.9,
     color : [ 1.0, 153/255, 0.0, 1.0 ]
   },
   Saturn : {
     radius : 0.60268,
-    distance : 14.27,
-    year : 29.4,
+    distance : 14.3,
+    year : 20.4,
     color : [ 1.0, 1.0, 0.0, 1.0 ]
   },
   Uranus : {
     radius : 0.25559,
-    distance : 28.710,
-    year : 83.7,
+    distance : 28.7,
+    year : 28.7,
     color : [ 0.0, 0.1, 1.0, 1.0 ]
   },
   Neptune : {
     radius : 0.24764,
-    distance : 44.971,
-    year : 163.7,
+    distance : 44.9
+    year : 34.7,
     color : [ 0.0, 0.0, 1.0, 1.0 ]
   },
   Pluto : {
     radius : 0.1195,
-    distance : 59.13,
-    year : 247.9,
+    distance : 59.1,
+    year : 47.9,
     color : [ 150/255, 75/255, 0.0, 1.0 ]
   }
 };
