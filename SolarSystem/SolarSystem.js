58f61a323f900d40c5b003e8a1463ded6e1cfe2e
edgarttrr <35516028+edgarttrr@users.noreply.github.com>
2018-02-22 10:27:42 -0800

    Update SolarSystem.js
    Planets still hitting each other updated distances again.

--- SolarSystem/SolarSystem.js
+++ SolarSystem/SolarSystem.js
@@ -13,13 +13,13 @@ var SolarSystem = {
   },
   Venus : {
     radius : 0.60518,
-    distance : 6.0,
+    distance : 8.0,
     year : 0.615,
     color : [ 1.0, 0.0, 1.0, 1.0 ]
   },
   Earth : {
     radius : 0.6378,
-    distance : 7.0,
+    distance : 12.0,
     year : 1,
     color : [ 0.0, 0.0, 1.0, 1.0 ]
   },
@@ -31,37 +31,37 @@ var SolarSystem = {
   },
   Mars : {
     radius : 0.3396,
-    distance : 8.0,
+    distance : 16.0,
     year : 1.88,
     color : [ 1.0, 0.0, 0.0, 1.0 ]
   },
   Jupiter : {
     radius : 0.71492,
-    distance : 12.0,
+    distance : 22.0,
     year : 11.9,
     color : [ 1.0, 153/255, 0.0, 1.0 ]
   },
   Saturn : {
     radius : 0.60268,
-    distance : 24.0,
+    distance : 34.0,
     year : 29.4,
     color : [ 1.0, 1.0, 0.0, 1.0 ]
   },
   Uranus : {
     radius : 0.25559,
-    distance : 38.0,
+    distance : 48.0,
     year : 83.7,
     color : [ 0.0, 0.1, 1.0, 1.0 ]
   },
   Neptune : {
     radius : 0.24764,
-    distance : 44.0,
+    distance : 54.0,
     year : 163.7,
     color : [ 0.0, 0.0, 1.0, 1.0 ]
   },
   Pluto : {
     radius : 0.1195,
-    distance : 59.0,
+    distance : 69.0,
     year : 247.9,
     color : [ 150/255, 75/255, 0.0, 1.0 ]
   }
