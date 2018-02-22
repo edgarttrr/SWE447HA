0ca98fc2a6555bf44a5a97e350fa46b86cd2841f
edgarttrr <35516028+edgarttrr@users.noreply.github.com>
2018-02-21 17:58:52 -0800

    Update SolarSystem.js
    Updated distance of planets in Kilo Meter scale.

--- SolarSystem/SolarSystem.js
+++ SolarSystem/SolarSystem.js
@@ -7,19 +7,19 @@ var SolarSystem = {
   },
   Mercury : {
     radius : 0.24397,
-    distance : 5.79,
+    distance : .0579,
     year : 0.241,
     color : [ 1.0, 0.0, 0.0, 1.0 ]
   },
   Venus : {
     radius : 0.60518,
-    distance : 10.82,
+    distance : .1082,
     year : 0.615,
     color : [ 1.0, 0.0, 1.0, 1.0 ]
   },
   Earth : {
     radius : 0.6378,
-    distance : 14.96,
+    distance : .1496,
     year : 1,
     color : [ 0.0, 0.0, 1.0, 1.0 ]
   },
@@ -31,37 +31,37 @@ var SolarSystem = {
   },
   Mars : {
     radius : 0.3396,
-    distance : 22.79,
+    distance : 2.279,
     year : 1.88,
     color : [ 1.0, 0.0, 0.0, 1.0 ]
   },
   Jupiter : {
     radius : 0.71492,
-    distance : 77.83,
+    distance : 7.783,
     year : 11.9,
     color : [ 1.0, 153/255, 0.0, 1.0 ]
   },
   Saturn : {
     radius : 0.60268,
-    distance : 142.70,
+    distance : 14.27,
     year : 29.4,
     color : [ 1.0, 1.0, 0.0, 1.0 ]
   },
   Uranus : {
     radius : 0.25559,
-    distance : 287.10,
+    distance : 28.710,
     year : 83.7,
     color : [ 0.0, 0.1, 1.0, 1.0 ]
   },
   Neptune : {
     radius : 0.24764,
-    distance : 444.971,
+    distance : 44.971,
     year : 163.7,
     color : [ 0.0, 0.0, 1.0, 1.0 ]
   },
   Pluto : {
     radius : 0.1195,
-    distance : 394.8,
+    distance : 59.13,
     year : 247.9,
     color : [ 150/255, 75/255, 0.0, 1.0 ]
   }
